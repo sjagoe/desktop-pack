cebccea6f53a063ab36a5b6889393da29f3d99c2
Simon Jagoe <sjagoe@enthought.com>
2013-05-18 17:48:36 +0100

    Fixed name

--- info.el
+++ info.el
@@ -1,3 +1,3 @@
-(live-pack-name "desktop-save-pack")
+(live-pack-name "desktop-pack")
 (live-pack-version "0.0.1")
-(live-pack-description "desktop-save-pack - Configure desktop-save-mode")
+(live-pack-description "desktop-pack - Configure desktop-save-mode")
